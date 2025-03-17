56d2e5e78ed75d24601d421bbc6dc047e9dc705f
martynweber <martyn.weber@bl4cksun.com>
2025-03-17 19:24:30 +0000

    reverted render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -23,9 +23,9 @@ fi
 $XDG_CACHE_HOME/hugo version
 
 # Render sets IS_PULL_REQUEST to true for PR previews.
-#if [ "${IS_PULL_REQUEST:-}" = "true" ]; then
-#    $XDG_CACHE_HOME/hugo --gc -e preview
-#else
-#    $XDG_CACHE_HOME/hugo --gc --minify
-#fi
-npm run build
+if [ "${IS_PULL_REQUEST:-}" = "true" ]; then
+    $XDG_CACHE_HOME/hugo --gc -e preview
+else
+    $XDG_CACHE_HOME/hugo --gc --minify
+fi
+npm version
